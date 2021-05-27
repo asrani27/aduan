3ce62b3e03f4f937e2526c888bd771ffa28f9296
asrani <asrani.27@gmail.com>
2021-05-27 09:18:32 +0800

    fix

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,76 @@
+<nav class="mt-2">
+<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+    <li class="nav-item">
+    <a href="/home" class="nav-link">
+        <i class="nav-icon fas fa-tachometer-alt"></i>
+        <p>
+        Beranda
+        </p>
+    </a>
+    </li>
+    <li class="nav-item">
+    <a href="/admin/aduan" class="nav-link">
+        <i class="nav-icon fas fa-comment-alt"></i>
+        <p>
+        Aduan Masuk
+        </p>
+    </a>
+    </li>
+    <li class="nav-item">
+    <a href="/admin/pegawai" class="nav-link">
+        <i class="nav-icon fas fa-users"></i>
+        <p>
+        Data Pegawai
+        </p>
+    </a>
+    </li>
+    <li class="nav-item">
+    <a href="/admin/customer" class="nav-link">
+        <i class="nav-icon fas fa-users"></i>
+        <p>
+        Data Customer
+        </p>
+    </a>
+    </li>
+    <li class="nav-item">
+    <a href="/admin/lap_aduan" class="nav-link">
+        <i class="nav-icon fas fa-file-alt"></i>
+        <p>
+        Laporan Aduan Masuk
+        </p>
+    </a>
+    </li>
+    <li class="nav-item">
+    <a href="/admin/lap_pegawai" class="nav-link">
+        <i class="nav-icon fas fa-file-alt"></i>
+        <p>
+        Laporan Pegawai
+        </p>
+    </a>
+    </li>
+    <li class="nav-item">
+    <a href="/admin/lap_kustomer" class="nav-link">
+        <i class="nav-icon fas fa-file-alt"></i>
+        <p>
+        Laporan Kustomer
+        </p>
+    </a>
+    </li>
+    <li class="nav-item">
+    <a href="/admin/lap_solusi" class="nav-link">
+        <i class="nav-icon fas fa-file-alt"></i>
+        <p>
+        Laporan Solusi
+        </p>
+    </a>
+    </li>
+    <li class="nav-item">
+    <a href="/logout" class="nav-link">
+        <i class="nav-icon fas fa-sign-out-alt"></i>
+        <p>
+        Logout
+        </p>
+    </a>
+    </li>
+</ul>
+</nav>
---
+++ routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AduanController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LaporanController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\KustomerController;
+use App\Http\Controllers\RegisterController;
+
+
+Route::get('/', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
+Route::post('/login', [LoginController::class, 'login']);
+Route::get('/login', function(){
+    return redirect('/');
+})->name('login');
+
+
+Route::group(['middleware' => ['auth', 'role:superadmin|user']], function () {
+    Route::get('/home', [HomeController::class, 'index']);
+    Route::get('/logout', [LoginController::class, 'logout']);
+});
+
+Route::group(['middleware' => ['auth', 'role:superadmin']], function () {
+    Route::prefix('admin')->group(function () {
+        Route::get('/aduan/respon/{id}', [AduanController::class, 'solusi']);
+        Route::get('/aduan/respon/{id}/delete', [AduanController::class, 'deleteSolusi']);
+        Route::post('/aduan/respon/{id}/solusi', [AduanController::class, 'storeSolusi']);
+        Route::resource('aduan', AduanController::class);
+        
+        Route::resource('pegawai', PegawaiController::class);
+        
+        Route::get('/customer', [KustomerController::class, 'index']);
+        Route::get('/lap_aduan', [LaporanController::class, 'aduan']);
+        Route::get('/lap_pegawai', [LaporanController::class, 'pegawai']);
+        Route::get('/lap_kustomer', [LaporanController::class, 'kustomer']);
+        Route::get('/lap_solusi', [LaporanController::class, 'solusi']);
+        
+        Route::get('/lap_aduan_pdf', [LaporanController::class, 'printaduan']);
+        Route::get('/lap_pegawai_pdf', [LaporanController::class, 'printpegawai']);
+        Route::get('/lap_kustomer_pdf', [LaporanController::class, 'printkustomer']);
+        Route::get('/lap_solusi_pdf', [LaporanController::class, 'printsolusi']);
+    });
+});
+
+Route::group(['middleware' => ['auth', 'role:user']], function () {
+    Route::prefix('user')->group(function () {
+        Route::get('/aduan', [UserController::class, 'aduan']);
+        Route::get('/aduan/create', [UserController::class, 'createAduan']);
+        Route::get('/aduan/respon/{id}', [UserController::class, 'respon']);
+        Route::post('/aduan/create', [UserController::class, 'storeAduan']);
+        Route::get('/aduan/destroy/{id}', [UserController::class, 'destroyAduan']);
+        Route::get('/aduan/edit/{id}', [UserController::class, 'editAduan']);
+        Route::post('/aduan/edit/{id}', [UserController::class, 'updateAduan']);
+        Route::post('/profil', [UserController::class, 'updateProfil']);
+    });
+});
